75fc7f089cfb31b269e431483487e63a6ab32520
atp-aung <76736708+atp-aung@users.noreply.github.com>
2025-04-15 22:19:35 +0630

    Add files via upload

---
+++ tsk__1_6/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const Header = (props) => {
+  return <h1>Give Feedback</h1>;
+};
+
+const Statistics = (props) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>good: {props.goodCount}</p>
+      <p>neutral: {props.neutralCount}</p>
+      <p>bad: {props.badCount}</p>
+    </>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.clickOp}>{props.text}</button>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodClick = () => {
+    setGood(good + 1);
+  };
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const badClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <>
+      <Header />
+      <Button clickOp={goodClick} text="good" />
+      <Button clickOp={neutralClick} text="neutral" />
+      <Button clickOp={badClick} text="bad" />
+      <Statistics goodCount={good} neutralCount={neutral} badCount={bad} />
+    </>
+  );
+};
+
+export default App;
